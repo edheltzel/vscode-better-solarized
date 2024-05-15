9eec8d21d3747c34ad316da7ee1b9f4955b55bc0
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-05-15 14:36:35 -0400

    adding env to config
    adds updated scripts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,17 @@
 /**
- * Flightdeck config powered by Eleventy
+ * Flightdeck powered by Eleventy
  * @module .eleventy
  * @see https://www.11ty.io/docs/config/
  * @param {Object} config
  */
 
+const isProd = process.env.ENV === "production";
+const addTransforms = require("./_flightdeck/transforms");
 module.exports = (config) => {
+	if (isProd) {
+		addTransforms(config);
+	}
+
 	return {
 		dir: {
 			input: "src",
