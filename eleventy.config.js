a4baa684c8cb7ed073f3cf53f50fbd971f0c6a02
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-05-15 13:01:45 -0400

    chore: Update Eleventy config directory paths

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,16 @@
 /**
- * Flightdeck - Eleventy Config
+ * Flightdeck config powered by Eleventy
  * @module .eleventy
  * @see https://www.11ty.io/docs/config/
  * @param {Object} config
  */
+
+module.exports = (config) => {
+	return {
+		dir: {
+			input: "src",
+			output: "test",
+			data: "data",
+		},
+	};
+};
