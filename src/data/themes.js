f84aca2b26efbb1da133e2fa94cfcfcf9f2d1a0a
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-05-15 13:02:34 -0400

    feat: Add themes data file includes sample data for testing

---
+++ src/data/themes.js
@@ -0,0 +1,33 @@
+const themes = {
+	semanticHighlighting: true,
+	solarizedDark: {
+		type: "dark",
+		black: "#f0f0f0",
+		white: "#333333",
+		red: "#C00C00",
+		blue: "#ff0000",
+	},
+	solarizedLight: {
+		type: "light",
+		black: "#f0f0f0",
+		white: "#333333",
+		red: "#C00C00",
+		blue: "#ff0000",
+	},
+	selenizedDark: {
+		type: "dark",
+		black: "#f0f0f0",
+		white: "#333333",
+		red: "#C00C00",
+		blue: "#ff0000",
+	},
+	selenizedLight: {
+		type: "light",
+		black: "#f0f0f0",
+		white: "#333333",
+		red: "#C00C00",
+		blue: "#ff0000",
+	},
+};
+
+module.exports = themes;
